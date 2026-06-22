2c32a1dff368341baad425a9a18bc34f7d0bc60d
krishnaGajabi <gajbikrishna23@gmail.com>
2026-06-22 04:51:16 +0000

    fix: sync buildHTTPClient with newHTTPClient in example
    Add proxy support and 5s resolver timeout to match the behaviour
    of newHTTPClient in the usage package.
    
    Signed-off-by: krishnaGajabi <gajbikrishna23@gmail.com>

--- example/main.go
+++ example/main.go
@@ -1,17 +1,59 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"time"
 
 	gaClient "github.com/openebs/google-analytics-4/pkg/client"
 	gaEvent "github.com/openebs/google-analytics-4/pkg/event"
 )
 
+func buildHTTPClient() (*http.Client, error) {
+	dns := os.Getenv("GA_DNS")
+	if dns == "" {
+		dialer := &net.Dialer{}
+		return &http.Client{
+			Transport: &http.Transport{
+				Proxy:       http.ProxyFromEnvironment,
+				DialContext: dialer.DialContext,
+			},
+		}, nil
+	}
+	if _, _, err := net.SplitHostPort(dns); err != nil {
+		return nil, fmt.Errorf("invalid GA_DNS address %q: must be host:port", dns)
+	}
+	dialer := &net.Dialer{
+		Resolver: &net.Resolver{
+			PreferGo: true,
+			Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+				d := net.Dialer{Timeout: 5 * time.Second}
+				return d.DialContext(ctx, network, dns)
+			},
+		},
+	}
+	return &http.Client{
+		Transport: &http.Transport{
+			Proxy:       http.ProxyFromEnvironment,
+			DialContext: dialer.DialContext,
+		},
+	}, nil
+}
+
 func main() {
+	httpClient, err := buildHTTPClient()
+	if err != nil {
+		panic(err)
+	}
+
 	client, err := gaClient.NewMeasurementClient(
 		gaClient.WithApiSecret("<api-secret>"),
 		gaClient.WithMeasurementId("<measurement-id>"),
 		gaClient.WithClientId("1b803d56-fde0-4f1e-ab64-ccb22509ae9f"),
+		gaClient.WithHttpClient(httpClient),
 	)
 	if err != nil {
 		panic(err)
